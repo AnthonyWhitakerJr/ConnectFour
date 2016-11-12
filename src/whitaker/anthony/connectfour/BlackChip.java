f3364cdd366e0b7f485d36ffb593d053dc1e90c8
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-11-12 01:22:09 -0500

    Extract duplicate chip code into base class

--- src/whitaker/anthony/connectfour/BlackChip.java
+++ src/whitaker/anthony/connectfour/BlackChip.java
@@ -6,32 +6,13 @@ package whitaker.anthony.connectfour;
 /**
  * @author Anthony R Whitaker
  */
-public class BlackChip implements GamePiece
+public class BlackChip extends Chip
 {
 	private Ellipse2D chip;
 
 	public BlackChip(Rectangle rect)
 	{
-		chip=new Ellipse2D.Double(rect.getX()+ rect.getWidth()*1/8, rect.getY()+ rect.getWidth()*1/8, rect.getWidth()*3/4, rect.getHeight()*3/4);
-	}	
-	
-	public void draw(Graphics2D g2)
-	{
-		g2.setColor(Color.BLACK);
-		g2.fill(chip);
-	}
-	public void move(int deltaY)
-	{
-		chip.setFrame(chip.getX(),chip.getY()+deltaY,chip.getWidth(),chip.getHeight());
-	}
-	public void setX(int x)
-	{
-		chip.setFrame(x,chip.getY(),chip.getWidth(),chip.getHeight());
-	}
-
-	public int getX()
-	{
-		return (int)chip.getX();
+		super(rect, Color.BLACK);
 	}
 
 }
