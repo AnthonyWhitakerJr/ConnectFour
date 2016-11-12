a3aa99775d507fe677f4566bcf93b1d4e33ad8ab
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-11-12 01:26:07 -0500

    Reformat code

--- src/whitaker/anthony/connectfour/C4.java
+++ src/whitaker/anthony/connectfour/C4.java
@@ -1,4 +1,5 @@
 package whitaker.anthony.connectfour;
+
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -6,40 +7,37 @@ import java.awt.image.BufferedImage;
 /**
  * @author Anthony R Whitaker
  */
-public class C4 extends Applet 
-{
+public class C4 extends Applet {
 
-	private static final int ROWS=6;
-	private static final int COLUMNS=7;
+	private static final int COLUMNS = 7;
+	private static final int ROWS = 6;
 	private BufferedImage backBuffer;
 	private Board board;
-	
-	public void init() 
-	{
-		Boolean focused=isFocusOwner();
-		board=new Board(ROWS, COLUMNS, getSize(), this,focused);
-		backBuffer=new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);		
+
+	public void init() {
+		Boolean focused = isFocusOwner();
+		board = new Board(ROWS, COLUMNS, getSize(), this, focused);
+		backBuffer = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
 		addMouseListener(board);
 		addFocusListener(board);
 		addMouseMotionListener(board);
 		addKeyListener(board);
 	}
 
-	public void paint(Graphics g) 
-	{
-		Graphics2D g2=(Graphics2D)backBuffer.getGraphics();
-		
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D)backBuffer.getGraphics();
+
 		//fill background
 		g2.setColor(Color.BLUE);
-		g2.fillRect(0,0,getWidth(),getHeight());
-		
+		g2.fillRect(0, 0, getWidth(), getHeight());
+
 		board.draw(g2);
-		g.drawImage(backBuffer,0,0,getWidth(),getHeight(),null);
+		g.drawImage(backBuffer, 0, 0, getWidth(), getHeight(), null);
 		g2.dispose();
 
 	}
-	public void update(Graphics g)
-	{
+
+	public void update(Graphics g) {
 		paint(g);
 	}
 
